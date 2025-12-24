7db6d52fb4586f81f1a45363dacdc171f37933bb
liamhunty <qmicctm768@outlook.com>
2025-12-24 18:15:43 +0800

    Wire deploy script for PrivOracle

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,13 +5,13 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const deployedPrivOracle = await deploy("PrivOracle", {
     from: deployer,
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  console.log(`PrivOracle contract: `, deployedPrivOracle.address);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_priv_oracle"; // id required to prevent reexecution
+func.tags = ["PrivOracle"];
